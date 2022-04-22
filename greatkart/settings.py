abf11aac7e02f2e04616a641d05c566203a61622
anh le <anhln@gmail.com>
2022-04-23 00:46:53 +0700

    fix missed load image from media when not debug

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -14,6 +14,7 @@ from ctypes import cast
 from pathlib import Path
 from decouple import config
 import dj_database_url
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -146,8 +147,11 @@ STATICFILES_DIRS = [
     BASE_DIR / 'greatkart/static',
 ]
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
