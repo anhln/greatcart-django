b424bcb46f0fc144c4e45b0642b38324b3432f1e
anh le <anhln@gmail.com>
2022-04-22 23:26:45 +0700

    edit databse url

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -87,13 +87,14 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {'default': dj_database_url.config(default='postgres://postgres:anhday123@localhost/greatecommerce')}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
